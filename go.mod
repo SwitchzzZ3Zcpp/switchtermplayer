461c91dc3ceab9b88262b336be482d43d4fe05fc
SwitchzzZ <91075627+SwitchzzZ3Zcpp@users.noreply.github.com>
2022-03-11 18:55:10 +0000

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,13 @@
+module mymodules
+
+go 1.17
+
+require (
+	github.com/hajimehoshi/go-mp3 v0.3.2 // indirect
+	github.com/hajimehoshi/oto v1.0.1 // indirect
+	github.com/mbndr/figlet4go v0.0.0-20190224160619-d6cef5b186ea // indirect
+	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
+	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
+	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
+	golang.org/x/sys v0.0.0-20190429190828-d89cdac9e872 // indirect
+)
---
+++ main.go
@@ -0,0 +1,89 @@
+package main 
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"log"
+	"github.com/hajimehoshi/oto"
+	"github.com/hajimehoshi/go-mp3"
+	"github.com/mbndr/figlet4go"
+	"io"	
+	// "strings"
+)
+
+//var mmtop string
+
+
+func main() {
+	arg := os.Args[1]
+
+	ascii := figlet4go.NewAsciiRender()
+
+	colorCyan := "\033[36m"
+
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//usr.HomeDir
+	renderStr, _ := ascii.Render("Hi " + usr.Username + " !")
+	//fmt.Println("Hi " + usr.Username + " !")
+	fmt.Print(string(colorCyan), renderStr)
+
+	f, err := os.Create("test.txt")
+	if err != nil {
+		panic(err)
+	} 
+		// fmt.Println("\nФайл был создан :)")
+	defer f.Close()
+
+	if arg == "help" {
+		fmt.Println("\nИспользование: switchp <путь к файлу/название файла>")
+	}  else {
+	f, err = os.Open(arg + ".mp3")
+	fmt.Println("I'm playing '" + arg + "' for you :)")
+	defer f.Close()
+
+	d, err := mp3.NewDecoder(f)
+	if err != nil {
+		panic(err)
+	}
+	
+	c, err := oto.NewContext(d.SampleRate(), 2, 2, 8192)
+	
+	defer c.Close()
+
+	p := c.NewPlayer()
+	defer p.Close()
+
+	if _, err := io.Copy(p, d); err != nil {
+	
+	}
+}
+	
+	//os.Remove("test.txt")
+
+	// var t string
+	// t = "Glory to Russia!"
+
+	// for i := 0; i < 9999; i++ {
+		// f.WriteString(t)
+	// }
+	// fmt.Println("I wrote ->\t" + t)
+
+	// fmt.Println(string(colorCyan), "\nGlory to Russia!")
+
+
+	//fmt.Println("Enter the name of mp3: ")
+	//fmt.Scanln(&arg)
+
+	// if err != nil {
+		// }
+	// if err := run(); err != nil {
+		// log.Fatal(err)
+	// }	
+}
+
+
